2022ec4c868e0f1112ed94b94915286ad02e3d56
Taylor Huston <taylor.a.huston@gmail.com>
2016-01-27 19:14:12 -0700

    Create Contact Us page with an AngularJS assisted form

--- Course 3/conFusion/app/scripts/app.js
+++ Course 3/conFusion/app/scripts/app.js
@@ -5,7 +5,7 @@ angular.module('confusionApp', [])
   $scope.tab = 1;
   $scope.filtText = '';
   $scope.showDetails = false;
-  
+
   $scope.dishes = [
     {
       name: 'Uthapizza',
@@ -57,14 +57,62 @@ angular.module('confusionApp', [])
       $scope.filtText = "";
     }
   };
-  
+
   $scope.isSelected = function (checkTab) {
     return ($scope.tab === checkTab);
   };
-  
-  $scope.ToggleDetails = function() {
+
+  $scope.ToggleDetails = function () {
     $scope.showDetails = !$scope.showDetails;
     console.log('clicked');
   };
-  
+
+}])
+
+.controller('ContactController', ['$scope', function ($scope) {
+
+  $scope.feedback = {
+    mychannel: "",
+    firstName: "",
+    lastName: "",
+    agree: false,
+    email: ""
+  };
+
+  var channels = [{
+    value: "tel",
+    label: "Tel."
+  }, {
+    value: "Email",
+    label: "Email"
+  }];
+  $scope.channels = channels;
+  $scope.invalidChannelSelection = false;
+
+}])
+
+.controller('FeedbackController', ['$scope', function ($scope) {
+  $scope.sendFeedback = function () {
+    console.log($scope.feedback);
+
+    if ($scope.feedback.agree && ($scope.feedback.mychannel == "") && !$scope.feedback.mychannel) {
+      $scope.invalidChannelSelection = true;
+      console.log('incorrect');
+    } else {
+      $scope.invalidChannelSelection = false;
+      $scope.feedback = {
+        mychannel: "",
+        firstname: "",
+        lastname: "",
+        agree: false,
+        email: ""
+      };
+      $scope.feedback.mychannel = "";
+
+      $scope.feedbackForm.$setPristine();
+      console.log($scope.feedback);
+    }
+  }
+
+
 }]);
